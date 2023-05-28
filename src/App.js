8f2f548f70b4f6804b1ac837c13a6548c3a54d34
Michael Kawata <michaelkawata@Bens-MacBook-Air.local>
2023-05-28 15:18:48 -0700

    Basic setup and styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.scss';
+import { Route, Routes } from 'react-router-dom'
+import Layout from "./assets/component/Layout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <h1>Hello</h1>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+      </Routes>
     </div>
   );
 }
