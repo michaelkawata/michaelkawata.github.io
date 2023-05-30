a58ac63887b15c96cb0ac718202d69577c345233
Michael Kawata <michaelkawata@bens-air.lan>
2023-05-30 15:59:00 -0700

    Home page

---
+++ src/components/Home/Logo/index.js
@@ -0,0 +1,14 @@
+import './index.scss'
+import LogoM from '../../../assets/images/logo-m1shadow.png'
+
+const Logo = () => {
+
+  return (
+    <div className="logo-container">
+      <img className="solid-logo" src={LogoM} alt='M' />
+    </div>
+  )
+}
+
+
+export default Logo
